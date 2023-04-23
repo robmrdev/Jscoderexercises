ead4020267d9180fcc08a471327b8aa5d17b931f
Robert Mendoza <robmrivas@gmail.com>
2023-04-23 19:32:28 -0300

    terceraEntrega

--- javascript/script.js
+++ javascript/script.js
@@ -1,111 +1,112 @@
-// Definimos las preguntas y respuestas
 const preguntas = [
     {
-    pregunta: "¿Cuál es tu asignatura favorita?",
-    opciones: [
-        "Defensa contra las artes oscuras",
-        "Pociones",
-        "Adivinacion",
-        "Herbologia",
-    ],
-    },
-    {
-    pregunta: "¿Cuál es tu animal favorito?",
-    opciones: [
-        "Leon",
-        "Serpiente",
-        "Aguila",
-        "Tejon"
-    ],
-    },
-    {
-    pregunta: "¿Qué cualidad valoras más?",
-    opciones: [
-        "Valentia",
-        "Astucia",
-        "Inteligencia",
-        "Lealtad"
-    ],
-    },
-    {
-    pregunta: "¿Qué lugar del mundo mágico te gustaría visitar?",
-    opciones:[
-        "Hogsmeade",
-        "El Callejón Knockturn",
-        "El Ministerio de Magia",
-        "La reserva Natural de Dragones"
-    ]
-    },
-    {
-        pregunta: "¿Con cuál de estas criaturas mágicas te identificas más?",
+        pregunta: "¿Cuál es tu asignatura favorita?",
+        opciones: [
+            "Defensa contra las artes oscuras",
+            "Pociones",
+            "Adivinacion",
+            "Herbologia",
+        ],
+        },
+        {
+        pregunta: "¿Cuál es tu animal favorito?",
+        opciones: [
+            "Leon",
+            "Serpiente",
+            "Aguila",
+            "Tejon"
+        ],
+        },
+        {
+        pregunta: "¿Qué cualidad valoras más?",
+        opciones: [
+            "Valentia",
+            "Astucia",
+            "Inteligencia",
+            "Lealtad"
+        ],
+        },
+        {
+        pregunta: "¿Qué lugar del mundo mágico te gustaría visitar?",
         opciones:[
-            "Hipogrifo",
-            "Mantícora",
-            "Esfinge",
-            "Centauro"
+            "Hogsmeade",
+            "El Callejón Knockturn",
+            "El Ministerio de Magia",
+            "La reserva Natural de Dragones"
         ]
-    }
-];
-
-// Función que muestra la pregunta y opciones al usuario
-function mostrarPregunta(pregunta) {
-let mensaje = pregunta.pregunta + "\n";
-pregunta.opciones.forEach((opcion, indice) => {
-    mensaje += `${indice + 1}. ${opcion}\n`;
-});
-
-let respuesta;
-do {
-    respuesta = prompt(mensaje);
-        if (!["1", "2", "3", "4"].includes(respuesta)) { // Validación modificada
-            alert("Esta no es una de las opciones! Selecciona una.");
+        },
+        {
+            pregunta: "¿Con cuál de estas criaturas mágicas te identificas más?",
+            opciones:[
+                "Hipogrifo",
+                "Mantícora",
+                "Esfinge",
+                "Centauro"
+            ]
         }
-    } while (!["1", "2", "3", "4"].includes(respuesta));
-
-return respuesta;
-}
-
-// Función principal que ejecuta el quiz
-function hacerQuiz() {
+];
+const questionEl = document.getElementById("question");
+const optionsEl = document.getElementById("options");
+let currentQuestion = 0;
 const respuestas = [];
-alert("A continuación empezaremos con el momento que definirá tu vida!\nAhora descubrirás a qué casa de Hogwarts perteneces.\nSelecciona las opciones que se te presentarán con los números correspondientes.")
-preguntas.forEach((pregunta) => {
-const respuesta = mostrarPregunta(pregunta);
-respuestas.push(respuesta);
-});
 
-// Determinamos la casa ganadora
-const casas = {
-    Gryffindor: 0,
-    Slytherin: 0,
-    Ravenclaw: 0,
-    Hufflepuff: 0,
-};
-
-respuestas.forEach((respuesta) => {
-    switch (respuesta) {
-        case "1":
-        casas.Gryffindor++;
-        break;
-        case "2":
-        casas.Slytherin++;
-        break;
-        case "3":
-        casas.Ravenclaw++;
-        break;
-        case "4":
-        casas.Hufflepuff++;
-        break;
-        default:
-        break;
-    }
-});
-
-// Ordenamos las casas por puntaje y mostramos la ganadora
-const casasOrdenadas = Object.entries(casas).sort((a, b) => b[1] - a[1]);
-const casaGanadora = casasOrdenadas[0][0];
-alert(`Fuiste seleccionad@ para la casa ${casaGanadora}!!`);
+function loadQuestion() {
+    const pregunta = preguntas[currentQuestion];
+    questionEl.textContent = pregunta.pregunta;
+    optionsEl.innerHTML = "";
+    pregunta.opciones.forEach((opcion, indice) => {
+        const option = document.createElement("button");
+        option.textContent = opcion;
+        option.onclick = () => checkAnswer(indice + 1);
+        optionsEl.appendChild(option);
+    });
 }
 
-// Ejecutamos la función principal
-hacerQuiz();
+function checkAnswer(selected) {
+    respuestas.push(selected);
+    currentQuestion++;
+    if (currentQuestion < preguntas.length) {
+        loadQuestion();
+    } else {
+        finishQuiz();
+    }
+}
+
+function finishQuiz() {
+    const casas = {
+        Gryffindor: 0,
+        Slytherin: 0,
+        Ravenclaw: 0,
+        Hufflepuff: 0,
+    };
+
+    respuestas.forEach((respuesta) => {
+        switch (respuesta) {
+            case 1:
+                casas.Gryffindor++;
+                break;
+            case 2:
+                casas.Slytherin++;
+                break;
+            case 3:
+                casas.Ravenclaw++;
+                break;
+            case 4:
+                casas.Hufflepuff++;
+                break;
+            default:
+                break;
+        }
+    });
+
+    const casasOrdenadas = Object.entries(casas).sort((a, b) => b[1] - a[1]);
+    const casaGanadora = casasOrdenadas[0][0];
+//guardamos el resultado en el local Storage
+localStorage.setItem("casaGanadora", casaGanadora);
+
+// Agregamos el resultado al DOM
+const resultadoDiv = document.getElementById("resultado");
+resultadoDiv.innerHTML = `Fuiste seleccionad@ para la casa ${localStorage.getItem("casaGanadora")}!!`;
+}
+
+loadQuestion();
